d2ef624a51c9cd3e3c2eb395124d8abe11e925fc
magn7015 <magn7015@edu.easj.dk>
2020-03-11 16:14:59 +0100

    Commit (works)

---
+++ ModelLib/Model/Book.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ModelLib.Model
+{
+    public class Bog
+    {
+        private string _forfatter;
+        private int _sidetal;
+        private string _isbn13;
+        public string Titel { get; set; }
+
+        public Bog(string titel, string forfatter, int sidetal, string isbn13)
+        {
+            Titel = titel;
+            Forfatter = forfatter;
+            Sidetal = sidetal;
+            Isbn13 = isbn13;
+        }
+
+        public Bog()
+        {
+
+        }
+
+
+        public string Forfatter
+        {
+            get => _forfatter;
+            set
+            {               
+                if (value.Length < 2) throw new ArgumentException();
+                _forfatter = value;
+            }
+
+        }
+
+        public int Sidetal
+        {
+            get => _sidetal;
+            set
+            {
+                if (value <= 4) throw new ArgumentOutOfRangeException();
+                if (value >= 1000) throw new ArgumentOutOfRangeException();
+                _sidetal = value;
+            }
+        }
+
+        public string Isbn13
+        {
+            get => _isbn13;
+
+            set
+
+            {
+                if (value.Length == 13) _isbn13 = value;
+                else throw new ArgumentOutOfRangeException();
+                _isbn13 = value;
+            }
+        }
+    }
+}
+
+
+
---
+++ Opgave4/Controllers/BookController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelLib.Model;
+
+namespace Opgave4.Controllers
+{ 
+    [Route("api/localbooks")]
+    [ApiController]
+    public class Book1Controller : ControllerBase
+    {
+        private static readonly List<Bog> books = new List<Bog>()
+        {
+            new Bog("Bog1", "Forfatter1", 114, "3928375647382"),
+            new Bog("Bog2", "Forfatter2", 25, "1928362392012"),
+            new Bog("Bog3", "Forfatter3", 10, "1173844958473"),
+            new Bog("Bog4", "Forfatter4", 593, "8392037352718"),
+            new Bog("bog5", "Forfatter5", 725, "8302936251627")
+        };
+
+        // GET
+        [HttpGet]
+        public IEnumerable<Bog> Get()
+        {
+            return books;
+
+        }
+
+        //GET
+        [HttpGet]
+        [Route("{isbn13}")]
+        public Bog Get(string isbn13)
+        {
+            return books.Find(i => i.Isbn13 == isbn13);
+        }
+
+        // POST
+        [HttpPost]
+        public void Post([FromBody] Bog value)
+        {
+            books.Add(value);
+        }
+
+        // PUT
+        [HttpPut]
+        [Route("{isbn13}")]
+        public void Put(string isbn13, [FromBody] Bog value)
+        {
+            Bog book = Get(isbn13);
+            if (book != null)
+            {
+                book.Titel = value.Titel;
+                book.Forfatter = value.Forfatter;
+                book.Sidetal = value.Sidetal;
+                book.Isbn13 = value.Isbn13;
+            }
+        }
+
+        // DELETE
+        [HttpDelete]
+        [Route("{isbn13}")]
+        public void Delete(string isbn13)
+        {
+            Bog book = Get(isbn13);
+            books.Remove(book);
+        }
+    }
+}
